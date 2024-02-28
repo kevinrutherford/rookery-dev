8a0a5f5aad96e022d795dc0a07321ef0424a9548
Kevin Rutherford <kevin@rutherford-software.com>
2024-02-28 16:02:34 +0000

    Exit early if a curl command fails

--- scripts/create-test-data.sh
+++ scripts/create-test-data.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 
-set -e -x
+set -e
 
 COLLECTION_1_ID=`uuidgen`
 COLLECTION_2_ID=`uuidgen`
 
-curl -X POST --url http://localhost:44001/collections -d '{
+curl --fail-with-body -X POST --url http://localhost:44001/collections -d '{
   "id": "'$COLLECTION_1_ID'",
   "handle": "chs",
   "name": "CHS",
   "description": "Papers under review by the CHS project"
 }'
 
-curl -X POST --url http://localhost:44001/collections -d '{
+curl --fail-with-body -X POST --url http://localhost:44001/collections -d '{
   "id": "'$COLLECTION_2_ID'",
   "handle": "pru3",
   "name": "PRU3",
