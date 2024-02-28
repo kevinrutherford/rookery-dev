07a295f4ae01447ed80791f2cb424a5b364ab82c
Kevin Rutherford <kevin@rutherford-software.com>
2024-02-28 16:28:35 +0000

    Ensure the API parses the body correctly

--- scripts/create-test-data.sh
+++ scripts/create-test-data.sh
@@ -5,14 +5,14 @@ set -e
 COLLECTION_1_ID=`uuidgen`
 COLLECTION_2_ID=`uuidgen`
 
-curl --fail-with-body -X POST --url http://localhost:44001/collections -d '{
+curl --fail-with-body -X POST -H 'Content-type: application/json' --url http://localhost:44001/collections -d '{
   "id": "'$COLLECTION_1_ID'",
   "handle": "chs",
   "name": "CHS",
   "description": "Papers under review by the CHS project"
 }'
 
-curl --fail-with-body -X POST --url http://localhost:44001/collections -d '{
+curl --fail-with-body -X POST -H 'Content-type: application/json' --url http://localhost:44001/collections -d '{
   "id": "'$COLLECTION_2_ID'",
   "handle": "pru3",
   "name": "PRU3",
