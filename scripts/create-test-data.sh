2cb0f4d52e486ececb60c5cc33617510951e2428
Kevin Rutherford <kevin@rutherford-software.com>
2024-03-04 15:45:07 +0000

    Allow any Rookery API to be populated with test data #2

--- scripts/create-test-data.sh
+++ scripts/create-test-data.sh
@@ -2,17 +2,19 @@
 
 set -e
 
+API="${1:-http://localhost:44001}"
+
 COLLECTION_1_ID=collection.`uuidgen`
 COLLECTION_2_ID=collection.`uuidgen`
 
-curl --fail-with-body -X POST -H 'Content-type: application/json' --url http://localhost:44001/collections -d '{
+curl --fail-with-body -X POST -H 'Content-type: application/json' --url ${API}/collections -d '{
   "id": "'$COLLECTION_1_ID'",
   "handle": "chs",
   "name": "CHS",
   "description": "Papers under review by the CHS project"
 }'
 
-curl --fail-with-body -X POST -H 'Content-type: application/json' --url http://localhost:44001/collections -d '{
+curl --fail-with-body -X POST -H 'Content-type: application/json' --url ${API}/collections -d '{
   "id": "'$COLLECTION_2_ID'",
   "handle": "pru3",
   "name": "PRU3",
@@ -25,37 +27,37 @@ ENTRY_3_ID=entry.`uuidgen`
 ENTRY_4_ID=entry.`uuidgen`
 ENTRY_5_ID=entry.`uuidgen`
 
-curl --fail-with-body -X POST -H 'Content-type: application/json' --url http://localhost:44001/entries -d '{
+curl --fail-with-body -X POST -H 'Content-type: application/json' --url ${API}/entries -d '{
   "id": "'$ENTRY_1_ID'",
   "doi": "10.1126/science.1172133",
   "collectionId": "'$COLLECTION_1_ID'"
 }'
 
-curl --fail-with-body -X POST -H 'Content-type: application/json' --url http://localhost:44001/entries -d '{
+curl --fail-with-body -X POST -H 'Content-type: application/json' --url ${API}/entries -d '{
   "id": "'$ENTRY_2_ID'",
   "doi": "10.3399/BJGP.2023.0216",
   "collectionId": "'$COLLECTION_1_ID'"
 }'
 
-curl --fail-with-body -X POST -H 'Content-type: application/json' --url http://localhost:44001/entries -d '{
+curl --fail-with-body -X POST -H 'Content-type: application/json' --url ${API}/entries -d '{
   "id": "'$ENTRY_3_ID'",
   "doi": "10.1111/padm.12268",
   "collectionId": "'$COLLECTION_2_ID'"
 }'
 
-curl --fail-with-body -X POST -H 'Content-type: application/json' --url http://localhost:44001/comments -d '{
+curl --fail-with-body -X POST -H 'Content-type: application/json' --url ${API}/comments -d '{
   "id": "'$(uuidgen)'",
   "entryId": "'$ENTRY_1_ID'",
   "content": "I love this!"
 }'
 
-curl --fail-with-body -X POST -H 'Content-type: application/json' --url http://localhost:44001/comments -d '{
+curl --fail-with-body -X POST -H 'Content-type: application/json' --url ${API}/comments -d '{
   "id": "'$(uuidgen)'",
   "entryId": "'$ENTRY_1_ID'",
   "content": "Personally, I think it is a little overrated."
 }'
 
-curl --fail-with-body -X POST -H 'Content-type: application/json' --url http://localhost:44001/comments -d '{
+curl --fail-with-body -X POST -H 'Content-type: application/json' --url ${API}/comments -d '{
   "id": "'$(uuidgen)'",
   "entryId": "'$ENTRY_3_ID'",
   "content": "Not relevant to our needs."
