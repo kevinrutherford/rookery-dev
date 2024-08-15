ce98a69a0c9cb382fc446cede8faf4a9dd40248d
Kevin Rutherford <kevin@rutherford-software.com>
2024-08-15 12:16:42 +0100

    Flesh out the main steps in the federation test

--- test/federation-test.sh
+++ test/federation-test.sh
@@ -1,8 +1,72 @@
 #! /bin/bash
 
-set -e
+set -ex
+
+#- config - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 . .env
+
+#- helpers - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+randomActor() {
+  echo 'abc'
+}
+
+get() {
+  curl --fail-with-body \
+    -X GET \
+    -H 'Accept: application/json' \
+    -H "Authorization: Bearer $1" \
+    --url $2
+}
+
+post() {
+  curl --fail-with-body \
+    -X POST \
+    -H 'Content-type: application/json' \
+    -H "Authorization: Bearer $1" \
+    --url $2 \
+    -d "$3"
+}
+
+create_collection() {
+  post `randomActor` ${COMMUNITY_A}/collections '{
+  "id": "'$COLLECTION_1_ID'",
+  "name": "Herbology",
+  "description": "Required reading for Hogwarts herbology classes."
+}'
+}
+
+assert_collection_appears_in_followers_feed() {
+  get `randomActor` ${COMMUNITY_B}/timelines/followed
+}
+
+start_discussion() {
+  post `randomActor` ${COMMUNITY_A}/entries '{
+  "id": "'$ENTRY_1_ID'",
+  "doi": "10.21203/rs.3.rs-4595783/v1",
+  "collectionId": "'$COLLECTION_1_ID'"
+}'
+}
+
+assert_discussion_appears_in_followers_feed() {
+  get `randomActor` ${COMMUNITY_B}/timelines/followed
+}
+
+add_comment() {
+  post `randomActor` ${COMMUNITY_A}/comments '{
+  "id": "'$(uuidgen)'",
+  "entryId": "'$ENTRY_1_ID'",
+  "content": "I love this!"
+}'
+}
+
+assert_comment_appears_in_followers_feed() {
+  get `randomActor` ${COMMUNITY_B}/timelines/followed
+}
+
+
+#- main - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 create_collection
 assert_collection_appears_in_followers_feed
